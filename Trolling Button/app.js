f96005c7004c903050a7612563a2352e54423398
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-12-11 17:04:59 +0700

    updating 11/12/22

---
+++ Trolling Button/app.js
@@ -0,0 +1,13 @@
+const b = document.querySelector('button.button--no')
+b.addEventListener("mouseover", moveHover)
+
+function moveHover(){
+    const i=Math.floor(Math.random()*50)+1;
+    const j=Math.floor(Math.random()*500)+1;
+
+    b.style.left=i+"px"
+    b.style.top=j+"px"
+
+}
+
+
